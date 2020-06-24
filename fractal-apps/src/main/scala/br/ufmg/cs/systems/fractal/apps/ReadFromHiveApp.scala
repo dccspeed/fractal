5e7acb2c1331ca875881e1c7f24a9e8702d3cdad
Cecília Assis <cy_assis@hotmail.com>
2020-06-24 00:18:45 -0300

    Log ReadFroHiveApp comands

--- fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ReadFromHiveApp.scala
+++ fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ReadFromHiveApp.scala
@@ -2,36 +2,39 @@ package br.ufmg.cs.systems.fractal.apps
 
 import java.util.UUID
 
+import br.ufmg.cs.systems.fractal.util.Logging
 import com.hortonworks.spark.sql.hive.llap.HiveWarehouseBuilder
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
-object ReadFromHiveApp {
+object ReadFromHiveApp extends Logging {
 
   def main(args: Array[String]) {
     // args
     val configPath = args(0)
     val config = ujson.read(scala.reflect.io.File(configPath).slurp)
 
-    val conf = new SparkConf()
-      .setAppName(config("app").str)
-      .setMaster(config("master").str)
-      .set("spark.sql.hive.hiveserver2.jdbc.url", config("url").str)
-      .set("spark.datasource.hive.warehouse.exec.results.max", Int.MaxValue.toString)
+    var conf = new SparkConf()
+    config("settings").arr.foreach(setting => {
+      conf = conf.set(setting("name").str, setting("value").str)
+    })
 
     val ss = SparkSession.builder.config(conf).enableHiveSupport().getOrCreate()
     val hive = HiveWarehouseBuilder.session(ss).build()
 
-    hive.execute(config("query").str)
-      .write
-      .csv(s"/dados01/ufmg.m06dcc/${UUID.randomUUID()}")
+    config("temporary_tables").arr.foreach(table => {
+      logInfo(s"\tLoading temporary table ${table("name").str} with query ${table("value").str}")
+      hive.execute(table("value").str).createOrReplaceTempView(table("name").str)
+    })
+
+    logInfo(s"\tLoading edges with query: ${config("query").str}")
+    val edges = hive.execute(config("query").str)
+
+    val outputPath = s"/dados01/ufmg.m06dcc/${UUID.randomUUID()}"
+    logInfo(s"\tWriting data to CSV at: ${outputPath}")
+    edges.write.csv(outputPath)
 
     hive.close()
     ss.stop()
   }
-
 }
-
-
-
-

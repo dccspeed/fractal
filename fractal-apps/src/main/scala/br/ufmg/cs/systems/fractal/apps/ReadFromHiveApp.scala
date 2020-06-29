eee75c142784f06494b0e7df58ae7dc71b6ce908
Cecília Assis <cy_assis@hotmail.com>
2020-06-28 22:01:49 -0300

    Use HiveApp class inside ReadFromHiveApp

--- fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ReadFromHiveApp.scala
+++ fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ReadFromHiveApp.scala
@@ -2,33 +2,26 @@ package br.ufmg.cs.systems.fractal.apps
 
 import java.util.UUID
 
+import br.ufmg.cs.systems.fractal.mpmg.HiveApp
 import br.ufmg.cs.systems.fractal.util.Logging
 import com.hortonworks.spark.sql.hive.llap.HiveWarehouseBuilder
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
 
 object ReadFromHiveApp extends Logging {
 
   def main(args: Array[String]) {
     // args
-    val configPath = args(0)
-    val config = ujson.read(scala.reflect.io.File(configPath).slurp)
+    val hiveApp = new HiveApp(args(0))
 
-    var conf = new SparkConf()
-    config("settings").arr.foreach(setting => {
-      conf = conf.set(setting("name").str, setting("value").str)
-    })
-
-    val ss = SparkSession.builder.config(conf).enableHiveSupport().getOrCreate()
+    val ss = hiveApp.updateSparkSession
     val hive = HiveWarehouseBuilder.session(ss).build()
 
-    config("temporary_tables").arr.foreach(table => {
+    hiveApp.databaseConfigs("temporary_tables").arr.foreach(table => {
       logInfo(s"\tLoading temporary table ${table("name").str} with query ${table("value").str}")
       hive.execute(table("value").str).createOrReplaceTempView(table("name").str)
     })
 
-    logInfo(s"\tLoading edges with query: ${config("query").str}")
-    val edges = hive.execute(config("query").str)
+    logInfo(s"\tLoading edges with query: ${hiveApp.databaseConfigs("query").str}")
+    val edges = hive.execute(hiveApp.databaseConfigs("query").str)
 
     val outputPath = s"/dados01/ufmg.m06dcc/${UUID.randomUUID()}"
     logInfo(s"\tWriting data to CSV at: ${outputPath}")

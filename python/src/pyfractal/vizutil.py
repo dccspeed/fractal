ed54efe6bbc3d2b35ae2cfd808c979e32244c838
viniciusvdias <vvsdias@gmail.com>
2024-07-29 13:43:58 -0300

    update

--- python/src/pyfractal/vizutil.py
+++ python/src/pyfractal/vizutil.py
@@ -4,7 +4,7 @@ from networkx.drawing.nx_agraph import graphviz_layout
 from itertools import count
 
 
-def draw_enumeration_tree_from_subgraphs(subgraphs, figsize, title=None, **kwargs):
+def draw_vertex_enumeration_tree_from_subgraphs(subgraphs, figsize, title=None, **kwargs):
     allpaths = [list(s.nodes()) for s in subgraphs]
     nodes = set()
     for p in allpaths:
@@ -44,8 +44,59 @@ def draw_enumeration_tree_from_subgraphs(subgraphs, figsize, title=None, **kwarg
     colors = [G.nodes[n]['vid'] for n in nodes]
     labels = nx.get_node_attributes(G, 'vid')
     pos = graphviz_layout(G, prog="dot")
-    nx.draw_networkx(G, pos, vmin=-1, vmax=max(groups), labels=labels, with_labels=True, node_color=colors,
+    nx.draw(G, pos, vmin=-1, vmax=max(groups), labels=labels, with_labels=True, node_color=colors,
                      cmap=plt.cm.Pastel1, linewidths=1, edgecolors='black', **kwargs)
+
+
+def draw_edge_enumeration_tree_from_subgraphs(subgraphs, figsize, title=None, **kwargs):
+    allpaths = [[f"{e[0]}-{e[1]}" for e in s.edges()] for s in subgraphs]
+    nodes = set()
+    for p in allpaths:
+        for u in p:
+            nodes.add(u)
+
+    node_to_id = {'-1-1': -1}
+    id_to_node = {-1: '-1-1'}
+    for n in nodes:
+        i = len(node_to_id)
+        node_to_id[n] = i
+        id_to_node[i] = n
+
+    num_nodes = len(nodes)
+    G = nx.DiGraph()
+    for root in sorted(nodes):
+        G.add_node('-1-1', vid=-1)
+        paths = [p for p in allpaths if p[0] == root]
+        paths_with_prefix = []
+        for p in paths:
+            prefix = []
+            ppfirst = (p[0], tuple(prefix))
+            pp = [ppfirst]
+            G.add_edge('-1-1', ppfirst)
+            for i in range(1, len(p)):
+                prefix.append(p[i - 1])
+                pp.append((p[i], tuple(prefix)))
+                paths_with_prefix.append(pp)
+
+        for p in paths_with_prefix:
+            for u in p:
+                G.add_node(u, vid=node_to_id[u[0]])
+            u = p[0]
+            for i in range(1, len(p)):
+                v = p[i]
+                G.add_edge(u, v)
+                u = v
+
+    plt.figure(figsize=figsize)
+    if title is not None:
+        plt.title(title)
+    groups = set(nx.get_node_attributes(G, 'vid').values())
+    nodes = G.nodes()
+    colors = [G.nodes[n]['vid'] for n in nodes]
+    labels = {n: id_to_node[i] for n, i in nx.get_node_attributes(G, 'vid').items()}
+    pos = graphviz_layout(G, prog="dot")
+    nx.draw(G, pos, vmin=-1, vmax=max(groups), labels=labels, with_labels=True, node_color=colors,
+            cmap=plt.cm.Pastel1, linewidths=1, edgecolors='black', **kwargs)
 
 
 def draw_fractal_graph(fg, figsize, prog="sfdp", title=None, color_mode="vid", **kwargs):
